78256a7dcefabe1310c214e432eef72be1dec0fb
DenisBode <Denis.Bode@gmx.de>
2026-06-08 22:06:21 +0200

    add images and rendering with categories

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-function renderFoods() {
+function renderFoods(selectedCategory) {
 
     let menuContent = document.getElementById("menuContent");
 
     menuContent.innerHTML = "";
 
     for (let i = 0; i < foods.length; i++) {
-
-        menuContent.innerHTML += getFoodTemplate(i);
-
+        if (foods[i].category === selectedCategory) {
+            menuContent.innerHTML += getFoodTemplate(i);
+        }
     }
 }
 
-renderFoods()
+renderFoods("burger");
