cb2b0123553d17eacc217d5bb881bab1d0f90cff
DenisBode <Denis.Bode@gmx.de>
2026-06-11 17:08:03 +0200

    optimize projekt strukture, add max size 1440px

--- scripts/basket.js
+++ scripts/basket.js
@@ -46,6 +46,18 @@ function renderCart() {
     cartContent.innerHTML = getCartTemplate(totalPrice, deliveryPrice);
 }
 
+function renderFoods(selectedCategory, containerId) {
+    let menuContent = document.getElementById(containerId);
+
+    menuContent.innerHTML = "";
+
+    for (let i = 0; i < foods.length; i++) {
+        if (foods[i].category === selectedCategory) {
+            menuContent.innerHTML += getFoodTemplate(i);
+        }
+    }
+}
+
 function isMobileView() {
     return window.matchMedia("(max-width: 900px)").matches;
 }
